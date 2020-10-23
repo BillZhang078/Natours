3aa6fcff760f2a8be30f8ed5eb71bb3fcd80e861
Bill78Zhang <wohencool21@gmail.com>
2020-10-23 15:50:22 +1030

    err handle middleware

--- app.js
+++ app.js
@@ -9,4 +9,21 @@ app.use(morgan('dev'));
 //Routes
 
 app.use('/api/v1/tours', tourRouter);
+
+app.use('*', (req, res, next) => {
+  const error = new Error(`Can't find the ${req.originalUrl} one this server`);
+  error.status = 'fail';
+  error.statusCode = 400;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  });
+});
 module.exports = app;
